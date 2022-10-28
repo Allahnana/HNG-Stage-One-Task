e1eaa09adcf1c7cc136865e5c63c01d4a4bc43c2
Allahnana <usmanallahnana@gmail.com>
2022-10-28 23:06:13 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+
 import './App.css';
+import profile from './image/abdul.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="sub-main">
+        <div>
+          <div className="img">
+            <div className="container-img">
+              <img src={profile} alt="dev_abdul" className="profile" />
+
+            </div>
+
+          </div>
+
+          <div className="profile-sec">
+            <h1 className="twitter">Dev_Allahnana</h1>
+            <h2 className="slack">Dev_Abdul</h2>
+          </div>
+
+          <div className="link-sec">
+
+            <button className="btn"><a className="link" href="https://training.zuri.team/">Zuri Team</a></button>
+            
+            <button className="btn"><a className="link" href="https://books.zuri.team/">Zuri Books</a></button>
+          
+            <button className="btn"><a className="link" href="https://books.zuri.team/">Python</a></button>
+            
+            <button className="btn"><a className="link" href="https://background.zuri.team/">Pitch</a></button>
+
+            <button className="btn"><a className="link" href="https://books.zuri.team/design-rules">Design</a></button>
+          </div>
+
+        </div>  
+      
+      </div>
     </div>
   );
 }
